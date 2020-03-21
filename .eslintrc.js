085a40b18595a907a26d29a985e7406ecf04e66b
wmarrujo <willmarrujo@gmail.com>
2020-03-20 20:46:01 -0500

    eslinting

---
+++ .eslintrc.js
@@ -0,0 +1,59 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/eslint-recommended"
+	],
+	parserOptions: {
+		"ecmaVersion": 2020,
+		"parser": "@typescript-eslint/parser",
+		"sourceType": "module"
+	},
+	plugins: ["@typescript-eslint"],
+	rules: {
+		"indent": ["error", "tab", {
+			"MemberExpression": 1,
+			"ArrayExpression": 1,
+			"ObjectExpression": 1,
+			"flatTernaryExpressions": true,
+			"VariableDeclarator": "first"
+		}],
+		"quotes": ["error", "double"],
+		"semi": ["error", "never"],
+		"no-console": ["warn", {"allow": ["warn", "error"]}],
+		"no-debugger": "warn",
+		"no-unused-vars": "off",
+		"@typescript-eslint/no-unused-vars": ["warn", {
+			"vars": "all",
+			"args": "after-used",
+			"ignoreRestSiblings": false
+		}],
+		"space-before-function-paren": ["error", {
+			"named": "never",
+			"anonymous": "always",
+			"asyncArrow": "always"
+		}],
+		"object-curly-spacing": ["error", "never"],
+		"array-bracket-spacing": ["error", "never"],
+		"computed-property-spacing": ["error", "never"],
+		"no-trailing-spaces": ["error", {
+			"skipBlankLines": true,
+			"ignoreComments": true
+		}],
+		"no-unneeded-ternary": "error",
+		"brace-style": ["error", "1tbs", {"allowSingleLine": true}],
+		"block-spacing": "error",
+		"camelcase": "error",
+		"comma-dangle": ["error", "never"],
+		"comma-spacing": ["error", {"before": false, "after": true}],
+		"comma-style": ["error", "last"],
+		"func-call-spacing": ["error", "never"],
+		"implicit-arrow-linebreak": ["error", "beside"],
+		"one-var": ["error", "never"],
+		"arrow-spacing": "error",
+		"no-var": "error"
+	}
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# quod-schedule-fundatur
+
+Evidence-based Scheduler & Predictor
+
+This package exports functions which take in task and schedule data and export a
+schedule and project predictions based on Joel Spolsky's
+[Evidence-Based Scheduling](https://www.joelonsoftware.com/2007/10/26/evidence-based-scheduling/) model.
