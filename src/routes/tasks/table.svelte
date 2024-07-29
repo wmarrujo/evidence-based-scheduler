761d1af6413d4ae9741ebff64c59529d1cee08b6
William Marrujo <willmarrujo@gmail.com>
2024-07-29 00:55:32 -0500

    make table nicer to use

--- src/routes/tasks/table.svelte
+++ src/routes/tasks/table.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
+	import {cn} from "$lib/utils"
 	import {db, liveQuery} from "$lib/db"
+	import type {Resource, ResourceId} from "$lib/db"
 	import * as Table from "$lib/components/ui/table"
 	import {createTable, Render, Subscribe, createRender} from "svelte-headless-table"
 	import {addPagination, addSortBy, addTableFilter, addHiddenColumns, addSelectedRows} from "svelte-headless-table/plugins"
 	import {Button} from "$lib/components/ui/button"
 	import {Input} from "$lib/components/ui/input"
-	import {derived} from "svelte/store"
+	import {derived, type Readable} from "svelte/store"
 	import TableCheckbox from "./table-checkbox.svelte"
 	import TableActions from "./table-actions.svelte"
 	import {ChevronDown, ArrowDownAZ, ArrowUpZA, ArrowDown01, ArrowUp10} from "lucide-svelte"
@@ -13,12 +15,25 @@
 	
 	////////////////////////////////////////////////////////////////////////////////
 	
-	let tasks = liveQuery(() => db.tasks.toArray())
-	let resources = liveQuery(() => db.tasks.toArray())
+	let tasks = derived(liveQuery(() => db.tasks.toArray()), ts => ts ?? [], [])
+	let resources = derived(liveQuery(() => db.resources.toArray()), rs => rs ?? [], [])
+	let resourcesById = derived(resources, resources => new Map(resources.map(r => [r.id, r])), new Map()) as Readable<Map<ResourceId, Resource>>
 	
-	// $: let resourcesById = resources
+	enum Status {
+		Planned = 0,
+		Started = 1,
+		Done = 2,
+	}
 	
-	const table = createTable(derived(tasks, ts => ts ?? [], []), {
+	let data = derived([tasks, resourcesById], ([$tasks, $resourcesById]) => {
+		return $tasks.map(task => ({
+			...task,
+			status: task.done ? Status.Done : task.spent <= 0 ? Status.Planned : Status.Started,
+			doerName: $resourcesById.get(task.doer)?.name ?? "ERROR: invalid doer",
+		}))
+	}, [])
+	
+	const table = createTable(data, {
 		page: addPagination(),
 		sort: addSortBy(),
 		filter: addTableFilter({
@@ -30,6 +45,7 @@
 	
 	const columns = table.createColumns([
 		table.column({
+			id: "checkbox",
 			accessor: "id",
 			header: (_, {pluginStates}) => {
 				const {allPageRowsSelected} = pluginStates.select
@@ -50,7 +66,8 @@
 			header: "Name",
 		}),
 		table.column({
-			accessor: "doer",
+			id: "doer",
+			accessor: "doerName",
 			header: "Doer",
 		}),
 		table.column({
@@ -72,6 +89,16 @@
 		table.column({
 			accessor: "done",
 			header: "Done",
+			cell: ({value}) => value ? "YES" : "NO", // TODO: make fancy chip for this
+			plugins: {
+				sort: {getSortValue: (value) => value ? 1 : 0},
+				filter: {exclude: true},
+			},
+		}),
+		table.column({
+			accessor: "status",
+			header: "Status",
+			cell: ({value}) => ["Planned", "Started", "Done"][value], // TODO: make fancy chip for this
 			plugins: {
 				filter: {exclude: true},
 			},
@@ -81,7 +108,8 @@
 		// projects
 		// milestones
 		table.column({
-			accessor: ({id}) => id,
+			id: "action",
+			accessor: "id",
 			header: "",
 			cell: ({value}) => createRender(TableActions, {task: value}),
 			plugins: {
@@ -99,9 +127,10 @@
 	const {selectedDataIds} = pluginStates.select
 	
 	const ids = flatColumns.map(col => col.id)
-	let hideColumnById = Object.fromEntries(ids.map(id => [id, true]))
+	let initialHiddenColumns = ["spent", "done"]
+	let hideColumnById = Object.fromEntries(ids.map(id => [id, !initialHiddenColumns.includes(id)]))
 	$: $hiddenColumnIds = Object.entries(hideColumnById).filter(([_, hide]) => !hide).map(([id]) => id)
-	const hidableColumns = ["estimate", "done"]
+	const hidableColumns = ["doer", "estimate", "spent", "done", "status"]
 </script>
 
 <div class="p-2">
@@ -128,27 +157,40 @@
 						<Table.Row>
 							{#each headerRow.cells as cell (cell.id)}
 								<Subscribe attrs={cell.attrs()} let:attrs props={cell.props()} let:props>
-									<Table.Head {...attrs} class="[&:has([role=checkbox])]:pl-3 p-1">
-										{#if cell.id == "id"}
-											<div class="pt-1">
+									<Table.Head {...attrs}
+										class={cn("[&:has([role=checkbox])]:pl-3 p-1",
+											["checkbox", "action", "estimate", "spent", "done", "status"].includes(cell.id) && "w-0", // shrink
+										)}
+									>
+										{#if cell.id == "checkbox"}
+											<div class="pt-1 pr-2">
 												<Render of={cell.render()} />
 											</div>
 										{:else if ["estimate", "spent"].includes(cell.id)}
 											<Button variant="ghost" on:click={props.sort.toggle} class="p-0 hover:bg-transparent w-full justify-end">
 												<Render of={cell.render()} />
 												{#if props.sort.order == "asc"}
-													<ArrowDown01 class="h-4 w-4" />
+													<ArrowDown01 class="ml-1 h-4 w-4" />
 												{:else if props.sort.order == "desc"}
-													<ArrowUp10 class="h-4 w-4" />
+													<ArrowUp10 class="ml-1 h-4 w-4" />
 												{/if}
 											</Button>
-										{:else if ["name", "doer", "done"].includes(cell.id)}
+										{:else if ["name", "doer"].includes(cell.id)}
 											<Button variant="ghost" on:click={props.sort.toggle} class="p-0 hover:bg-transparent w-full justify-start">
 												<Render of={cell.render()} />
 												{#if props.sort.order == "asc"}
-													<ArrowDownAZ class="h-4 w-4" />
+													<ArrowDownAZ class="ml-1 h-4 w-4" />
 												{:else if props.sort.order == "desc"}
-													<ArrowUpZA class="h-4 w-4" />
+													<ArrowUpZA class="ml-1 h-4 w-4" />
+												{/if}
+											</Button>
+										{:else if ["done", "status"].includes(cell.id)}
+											<Button variant="ghost" on:click={props.sort.toggle} class="p-0 hover:bg-transparent w-full justify-start">
+												<Render of={cell.render()} />
+												{#if props.sort.order == "asc"}
+													<ArrowDown01 class="ml-1 h-4 w-4" />
+												{:else if props.sort.order == "desc"}
+													<ArrowUp10 class="ml-1 h-4 w-4" />
 												{/if}
 											</Button>
 										{:else}
@@ -167,8 +209,8 @@
 						<Table.Row data-state={$selectedDataIds[row.id] && "selected"} {...rowAttrs}>
 							{#each row.cells as cell (cell.id)}
 								<Subscribe attrs={cell.attrs()} let:attrs>
-									<Table.Cell class="p-1" {...attrs}>
-										{#if cell.id == "id"}
+									<Table.Cell class={cn("p-1"/*, cell.id == "checkbox" && "w-min", cell.id == "action" && "w-10"*/)} {...attrs}>
+										{#if cell.id == "checkbox"}
 											<div class="pl-2 pt-1">
 												<Render of={cell.render()} />
 											</div>
@@ -176,10 +218,12 @@
 											<div class="text-right">
 												<Render of={cell.render()} /><span class="text-muted-foreground">h</span>
 											</div>
-										{:else}
-											<div class="">
+										{:else if cell.id == "action"}
+											<div>
 												<Render of={cell.render()} />
 											</div>
+										{:else}
+											<Render of={cell.render()} />
 										{/if}
 									</Table.Cell>
 								</Subscribe>
